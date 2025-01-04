3361c419d5573d66b855558496b195b1882664a8
Mariano <mtv890@users.noreply.github.com>
2025-01-03 22:08:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getHtmlBasedOnNumber(number) {
     const numberStr = number.toString();
     let html = '';
     for (let char of numberStr) {
-         html += `<img src="./counter${char}.gif" style="border: 0px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;" alt="${char}" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left="">`;
+         html += `<img src="./counter/${char}.gif" style="border: 0px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;" alt="${char}" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left="">`;
     }
     return html;
 }
