03dedd351ac225d7f0a909658ba832c151835335
Mariano <mtv890@users.noreply.github.com>
2025-01-03 22:04:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function getHtmlBasedOnNumber(number) {
     const numberStr = number.toString();
     let html = '';
-    for (let a of numberStr) {
-         html += '<img src="./counter/'+ a +'.gif" style="border: 0px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;" alt="'+a+'" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left="">';
+    for (let char of numberStr) {
+         html += `<img src="./counter/${char}.gif" style="border: 0px; --darkreader-inline-border-top: 0px; --darkreader-inline-border-right: 0px; --darkreader-inline-border-bottom: 0px; --darkreader-inline-border-left: 0px;" alt="${char}" data-darkreader-inline-border-top="" data-darkreader-inline-border-right="" data-darkreader-inline-border-bottom="" data-darkreader-inline-border-left="">`;
     }
     return html;
 }
